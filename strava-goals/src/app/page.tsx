db3360abbfd0a4e6d6fa9ffee386a91a15bb83a8
Stuart Clark <stuart.clark@jarden.co.nz>
2023-05-07 10:04:23 +1200

    build: initial commit of nextjs app

---
+++ strava-goals/src/app/page.tsx
@@ -0,0 +1,25 @@
+import {redirect} from "next/navigation";
+import SignOutButton from "@/components/SignOutButton";
+import {getServerCustomSession} from "@/shared/auth";
+import Stats from "@/components/Stats";
+
+
+export default async function Home() {
+
+  const session = await getServerCustomSession();
+     // console.debug({session});
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+  return (
+      <>
+          {/* @ts-expect-error Async Server Component */}
+            <Stats/>
+          <br/>
+        <p>You can view this page because you are signed in.</p>
+          <SignOutButton/>
+      </>
+  )
+
+}
