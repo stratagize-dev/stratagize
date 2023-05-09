eef6c608c27e315b810fcc9f00f0358e783befbd
Stuart Clark <stuart.clark@jarden.co.nz>
2023-05-09 17:40:36 +1200

    fix linting error

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { signOut } from 'next-auth/react';
 
-export default () => (
+const SignOutButton = () => (
   <button
     className="g-transparent hover:bg-orange-500 text-orange-500 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded"
     onClick={() => signOut()}
@@ -9,3 +9,5 @@ export default () => (
     Sign Out
   </button>
 );
+
+export default SignOutButton;
