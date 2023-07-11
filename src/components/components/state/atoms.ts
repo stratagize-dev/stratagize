32ceec770e5b8ff61bb1b0b924f1af3096d7e1d5
Stuart Clark <stuart.clark@jarden.co.nz>
2023-07-11 15:13:19 +1200

    feat:  wip

--- src/components/components/state/atoms.ts
+++ src/components/components/state/atoms.ts
@@ -1,6 +1,5 @@
 import { atomWithStorage } from 'jotai/utils';
 import { ActivitySummary } from '@/shared/types/ActivitySummary';
-import { atomWithLocalStorage } from '@/shared/atomWithLocalStorage';
 
 export const annualHourGoalAtom = atomWithStorage('annualHourGoalAtom', 365);
 
@@ -13,16 +12,3 @@ export const lastActivityDateAtom = atomWithStorage<string | undefined>(
   'lastActivityDateAtom',
   undefined
 );
-
-// const defaultStorage: Storage<unknown> = {
-//   getItem: (key) => {
-//     const storedValue = localStorage.getItem(key)
-//     if (storedValue === null) {
-//       throw new Error('no value stored')
-//     }
-//     return JSON.parse(storedValue)
-//   },
-//   setItem: (key, newValue) => {
-//     localStorage.setItem(key, JSON.stringify(newValue))
-//   },
-// }
