44fad4ae99c39630e01a87863c9b4c89ed24679e
stuart-clark-hatch <143360113+stuart-clark-hatch@users.noreply.github.com>
2023-09-27 12:20:48 +1300

    add in extra logging

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,78 +1,7 @@
 // noinspection JSUnusedGlobalSymbols
 
 import NextAuth from 'next-auth';
-import StravaProvider from 'next-auth/providers/strava';
-import { AuthOptions } from 'next-auth';
-import { CustomJWT } from '@/shared/types/auth/CustomJWT';
-import CustomSession from '@/shared/types/auth/CustomSession';
-import { refreshToken } from '@/shared/external/Strava/token/refreshToken';
-import sessionService from '@/shared/services/sessionService';
-
-export const authOptions: AuthOptions = {
-  providers: [
-    StravaProvider({
-      clientId: process.env.CLIENT_ID ?? '',
-      clientSecret: process.env.CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope: 'profile:read_all,activity:read_all'
-        }
-      }
-    })
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      const customJWT: CustomJWT = {
-        ...token
-      };
-      if (account) {
-        customJWT.accessToken = account.access_token;
-        customJWT.refreshToken = account.refresh_token;
-      }
-
-      if (Date.now() < Number(token.exp) * 1000) {
-        try {
-          const result = await refreshToken(customJWT.refreshToken ?? '');
-          customJWT.accessToken = result.accessToken;
-          customJWT.exp = result.expiry;
-          customJWT.refreshToken = result.refreshToken;
-        } catch (e) {
-          console.error(e);
-        }
-      }
-
-      return customJWT;
-    },
-    async session({ session, token }) {
-      // Send properties to the client, like an access_token and user id from a provider.
-      const customSession: CustomSession = {
-        ...session,
-        athleteId: token.sub,
-        accessToken: token.accessToken as string,
-        refreshToken: token.refreshToken as string
-      };
-      return customSession;
-    }
-  },
-  events: {
-    signIn: message => console.debug('event', 'signIn', { message }),
-    signOut: message => console.debug('event', 'signOut', { message }),
-    createUser: message => console.debug('event', 'createUser', { message }),
-    updateUser: message => console.debug('event', 'updateUser', { message }),
-    linkAccount: message => console.debug('event', 'linkAccount', { message }),
-    session: async message => {
-      const customSession = message.session as CustomSession;
-
-      if (customSession.athleteId && customSession.refreshToken) {
-        const athleteId = parseInt(customSession.athleteId);
-        await sessionService.beginSession(
-          athleteId,
-          customSession.refreshToken
-        );
-      }
-    }
-  }
-};
+import { authOptions } from '@/shared/auth';
 
 const handler = NextAuth(authOptions);
 
