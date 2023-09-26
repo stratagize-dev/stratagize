7287c40e5c4536149c5d842863e2c61f649a273b
stuart-clark-hatch <143360113+stuart-clark-hatch@users.noreply.github.com>
2023-09-27 12:22:23 +1300

    add in extra logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,28 @@ import Goal from '@/components/serverSide/Goal';
 import athleteRepository from '@/shared/repository/athleteRepository';
 
 export default async function Home() {
-  // const session = await getServerCustomSession();
-  // if (!session) {
-  //   redirect('/api/auth/signin');
-  // }
+  const session = await getServerCustomSession();
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
 
-  return <div>foo baa</div>;
-  // const athleteId = Number(session.athleteId);
-  // console.debug(`retrieving athlete ${session.athleteId}`);
-  // const { data: athlete } = await athleteRepository.get(athleteId);
-  //
-  // if (athlete) {
-  //   return (
-  //     <>
-  //       <div className="flex justify-between pt-5 pb-2 px-5 border-b">
-  //         <div className="text-orange-500 font-semibold py-2 px-4">
-  //           Strava Goals
-  //         </div>
-  //         <SignOutButton />
-  //       </div>
-  //       <Goal athlete={athlete} />
-  //     </>
-  //   );
-  // } else {
-  //   return <div>error finding athlete</div>;
-  // }
+  const athleteId = Number(session.athleteId);
+  console.debug(`retrieving athlete ${session.athleteId}`);
+  const { data: athlete } = await athleteRepository.get(athleteId);
+
+  if (athlete) {
+    return (
+      <>
+        <div className="flex justify-between pt-5 pb-2 px-5 border-b">
+          <div className="text-orange-500 font-semibold py-2 px-4">
+            Strava Goals
+          </div>
+          <SignOutButton />
+        </div>
+        <Goal athlete={athlete} />
+      </>
+    );
+  } else {
+    return <div>error finding athlete</div>;
+  }
 }
