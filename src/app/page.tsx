65009ad5d364cf5a3c31e616eff2c0de3159ec51
stuart-clark-hatch <143360113+stuart-clark-hatch@users.noreply.github.com>
2023-09-30 15:56:44 +1300

    fix suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,57 @@ import { Suspense } from 'react';
 import { activityService } from '@/shared/services/activityService';
 import Stats from '@/components/clientSide/components/Stats';
 
+async function ActivityLoading({
+  athleteId,
+  athleteHourGoal
+}: {
+  athleteId: number;
+  athleteHourGoal: number;
+}) {
+  const { data: activities } = await activityService.getActivitiesForAthlete(
+    athleteId
+  );
+
+  return (
+    <Suspense fallback={<div>loading activity data</div>}>
+      <Stats activities={activities ?? []} goalHours={athleteHourGoal} />;
+    </Suspense>
+  );
+}
+
+async function Onboarding({
+  athleteId,
+  session
+}: {
+  athleteId: number;
+  session: CustomSession;
+}) {
+  const { data: athlete } = await athleteRepository.get(athleteId);
+
+  if (!athlete) throw new Error('Athlete not found');
+
+  if (athlete?.is_onboarded === false && session.accessToken) {
+    await onboardAthlete(athlete, session.accessToken);
+  }
+
+  return (
+    <ClientSide session={session}>
+      <ActivityLoading
+        athleteId={athleteId}
+        athleteHourGoal={athlete.hour_goal}
+      />
+    </ClientSide>
+  );
+}
 export default async function Home() {
   const session: CustomSession =
     (await getServerCustomSession()) as CustomSession;
 
   const athleteId = Number(session.athleteId);
 
-  const { data: athlete } = await athleteRepository.get(athleteId);
-
-  if (athlete?.is_onboarded === false && session.accessToken) {
-    await onboardAthlete(athlete, session.accessToken);
-  }
-
-  const { data: activities } = await activityService.getActivitiesForAthlete(
-    athleteId
-  );
-
-  if (!athlete) throw new Error('Athlete not found');
   return (
     <Suspense fallback={<div>on boarding athlete.....</div>}>
-      <ClientSide session={session}>
-        <Stats activities={activities ?? []} goalHours={athlete.hour_goal} />
-      </ClientSide>
+      <Onboarding athleteId={athleteId} session={session} />{' '}
     </Suspense>
   );
 }
