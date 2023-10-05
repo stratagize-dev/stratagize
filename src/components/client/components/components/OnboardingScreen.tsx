c530c39888c92ff5384e138285bf16dc39088452
Stuart Clark <sjclark76@gmail.com>
2023-10-06 11:11:28 +1300

    fix up merge

--- src/components/client/components/components/OnboardingScreen.tsx
+++ src/components/client/components/components/OnboardingScreen.tsx
@@ -1,13 +1,8 @@
 import CustomSession from '@/shared/types/auth/CustomSession';
 import { onboardAthlete } from '@/shared/services/sessionService';
-<<<<<<<< HEAD:src/components/client/components/components/OnboardingScreen.tsx
 import ClientSide from '@/components/client/ClientSide';
 import ActivityLoadingScreen from '@/components/client/components/components/components/ActivityLoadingScreen';
 import { createAthletesRepository } from '@/shared/repository/athleteRepository';
-========
-import ActivityLoadingScreen from '@/components/server/components/ActivityLoadingScreen';
-import { Suspense } from 'react';
->>>>>>>> main:src/components/server/OnboardingScreen.tsx
 
 export default async function OnboardingScreen({
   athleteId,
@@ -26,12 +21,11 @@ export default async function OnboardingScreen({
   }
 
   return (
-    <Suspense fallback={<div>loading activity data for user</div>}>
+    <ClientSide session={session}>
       <ActivityLoadingScreen
         athleteId={athleteId}
         athleteHourGoal={athlete.hour_goal}
-        session={session}
       />
-    </Suspense>
+    </ClientSide>
   );
 }
--- src/components/client/components/components/components/ActivityLoadingScreen.tsx
+++ src/components/client/components/components/components/ActivityLoadingScreen.tsx
@@ -1,5 +1,4 @@
 import { activityService } from '@/shared/services/activityService';
-<<<<<<<< HEAD:src/components/client/components/components/components/ActivityLoadingScreen.tsx
 import { Suspense } from 'react';
 import Stats from '@/components/client/components/components/components/components/Stats';
 
@@ -11,34 +10,16 @@ export default async function ActivityLoadingScreen({
   athleteHourGoal: number;
 }) {
   const { data: activities } = await activityService().getActivitiesForAthlete(
-========
-import Stats from '@/components/client/Stats';
-import ClientSide from '@/components/client/ClientSide';
-import CustomSession from '@/shared/types/auth/CustomSession';
-
-interface Props {
-  athleteId: number;
-  athleteHourGoal: number;
-  session: CustomSession;
-}
-
-export default async function ActivityLoadingScreen({
-  athleteId,
-  athleteHourGoal,
-  session
-}: Props) {
-  const { data: activities } = await activityService.getActivitiesForAthlete(
->>>>>>>> main:src/components/server/components/ActivityLoadingScreen.tsx
     athleteId
   );
 
   return (
-    <ClientSide session={session}>
+    <Suspense fallback={<div>loading activity data</div>}>
       <Stats
         athleteId={athleteId}
         activities={activities ?? []}
         goalHours={athleteHourGoal}
       />
-    </ClientSide>
+    </Suspense>
   );
 }
