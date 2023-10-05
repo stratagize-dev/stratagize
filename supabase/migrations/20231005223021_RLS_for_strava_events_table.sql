7225d159bf8450efb0ee2681662a4d3227f7bf4d
Stuart Clark <sjclark76@gmail.com>
2023-10-06 11:32:09 +1300

    add missing migrations

---
+++ supabase/migrations/20231005223021_RLS_for_strava_events_table.sql
@@ -0,0 +1,15 @@
+drop trigger if exists "strava_events_trigger" on "public"."strava_events";
+
+alter table "public"."strava_events" enable row level security;
+
+create policy "Enable read access for all users"
+on "public"."strava_events"
+as permissive
+for all
+to public
+using (false);
+
+
+CREATE OR REPLACE TRIGGER strava_events_trigger AFTER INSERT ON public.strava_events FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('https://a48e-124-248-141-142.ngrok-free.app/api/strava-event', 'POST', '{"Content-type":"application/json"}', '{}', '1000');
+
+
