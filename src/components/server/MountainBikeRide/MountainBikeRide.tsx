180f53278db7d9602dcd7dfef8914d60e6e3df2a
Stuart Clark <sjclark76@gmail.com>
2023-11-21 08:11:56 +1300

    wip

--- src/components/server/MountainBikeRide/MountainBikeRide.tsx
+++ src/components/server/MountainBikeRide/MountainBikeRide.tsx
@@ -1,6 +1,7 @@
 import { Activity } from '@/shared/types/Activity';
 import { iterate } from '@/shared/services/sports/sportsService';
 import { calculateMovingTime } from '@/shared/services/statistics';
+import { time } from '@/shared/types/time';
 
 interface Props {
   activities: Activity.Row[];
@@ -10,9 +11,11 @@ export function MountainBikeRide({ activities }: Props) {
   const totalMovingTime = calculateMovingTime(activities);
   return (
     <div>
-      <div>MountainBikeRide</div>
+      <div>Mountain Bike</div>
       <div>longest activity: {result.longestActivity().human}</div>
-      <div>total moving time: {result.longestActivity().human}</div>
+      <div>
+        total moving time: {time(totalMovingTime.totalMovingTime)().human}
+      </div>
     </div>
   );
 }
