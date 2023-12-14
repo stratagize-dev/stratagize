973746e289c4879b9bd9d04990e6596e28378a23
Stuart Clark <sjclark76@gmail.com>
2023-12-15 09:56:03 +1300

    add pure onboarding page

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,5 @@
+export default async function Onboarding() {
+  return (
+    <div>We have started the onboarding process. This may take some time</div>
+  );
+}
--- src/components/client/components/components/OnboardingScreen.tsx
+++ src/components/client/components/components/OnboardingScreen.tsx
@@ -3,6 +3,7 @@ import { onboardAthlete } from '@/shared/services/sessionService';
 import ClientSide from '@/components/client/ClientSide';
 import ActivityLoadingScreen from '@/components/client/components/components/components/ActivityLoadingScreen';
 import { createAthletesRepository } from '@/shared/repository/athleteRepository';
+import { redirect } from 'next/navigation';
 
 export default async function OnboardingScreen({
   athleteId,
@@ -18,6 +19,7 @@ export default async function OnboardingScreen({
 
   if (athlete?.is_onboarded === false) {
     await onboardAthlete(athlete);
+    redirect('onboarding');
   }
 
   return (
