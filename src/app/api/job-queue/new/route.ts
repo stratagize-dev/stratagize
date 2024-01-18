df815ec33ccd02a3a24328d833c7a36f3ce9e8db
Stuart Clark <sjclark76@gmail.com>
2024-01-19 10:44:33 +1300

    i have no idea

--- src/app/api/job-queue/new/route.ts
+++ src/app/api/job-queue/new/route.ts
@@ -4,25 +4,31 @@ import { logDatabaseError } from '@/shared/logging/logDatabaseError';
 import { createJobQueueRepository } from '@/shared/repository/jobQueueRespository';
 import { batchSize } from '@/app/api/job-queue/constants';
 import { JobQueue } from '@/shared/types/JobQueue';
+import logError from '@/shared/logging/logError';
 
 export async function POST() {
-  console.log('looking for new jobs');
+  try {
+    console.log('looking for new jobs');
 
-  const jobsRepository = await createJobQueueRepository(serviceRoleDb);
-  const { data, error } = await jobsRepository.findByStatus('new', batchSize);
+    const jobsRepository = await createJobQueueRepository(serviceRoleDb);
+    const { data, error } = await jobsRepository.findByStatus('new', batchSize);
 
-  if (error) {
-    logDatabaseError('an error occured retrieving unprocessed jobs', error);
-    return NextResponse.error();
-  } else {
-    if (data) {
-      const processing = data?.map(
-        job => ({ ...job, status: 'processing' }) as JobQueue.Insert
-      );
-      const jobsRepository = await createJobQueueRepository(serviceRoleDb);
+    if (error) {
+      logDatabaseError('an error occured retrieving unprocessed jobs', error);
+      return NextResponse.error();
+    } else {
+      if (data) {
+        const processing = data?.map(
+          job => ({ ...job, status: 'processing' }) as JobQueue.Insert
+        );
+        const jobsRepository = await createJobQueueRepository(serviceRoleDb);
 
-      await jobsRepository.upsert(processing);
+        await jobsRepository.upsert(processing);
+      }
+      return NextResponse.json({ status: 'ok' });
     }
-    return NextResponse.json({ status: 'ok' });
+  } catch (e) {
+    logError('an unknown error occured trying process new jobs', e);
+    return NextResponse.error();
   }
 }
--- src/app/api/job-queue/retry/route.ts
+++ src/app/api/job-queue/retry/route.ts
@@ -7,37 +7,42 @@ import { JobHandlerPayload } from '@/app/api/job-handler/types';
 import logError from '@/shared/logging/logError';
 
 export async function POST() {
-  console.log('retrying failed jobs');
+  try {
+    console.log('retrying failed jobs');
 
-  const jobsRepository = await createJobQueueRepository(serviceRoleDb);
+    const jobsRepository = await createJobQueueRepository(serviceRoleDb);
 
-  const { data, error } = await jobsRepository.findByStatusAndDate(
-    'retry',
-    new Date(),
-    batchSize
-  );
+    const { data, error } = await jobsRepository.findByStatusAndDate(
+      'retry',
+      new Date(),
+      batchSize
+    );
 
-  if (error) {
-    logDatabaseError('an error occured retrieving failed jobs', error);
-    return NextResponse.error();
-  } else {
-    if (data) {
-      data.forEach(dataItem => {
-        if (dataItem.url_path) {
-          const payload: JobHandlerPayload<unknown> = {
-            jobId: dataItem.job_id,
-            payload: dataItem.payload
-          };
-          fetch(dataItem.url_path, {
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify(payload),
-            method: 'POST'
-          });
-        } else {
-          logError(`missing job url_path for job number ${dataItem.job_id}`);
-        }
-      });
+    if (error) {
+      logDatabaseError('an error occured retrieving failed jobs', error);
+      return NextResponse.error();
+    } else {
+      if (data) {
+        data.forEach(dataItem => {
+          if (dataItem.url_path) {
+            const payload: JobHandlerPayload<unknown> = {
+              jobId: dataItem.job_id,
+              payload: dataItem.payload
+            };
+            fetch(dataItem.url_path, {
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify(payload),
+              method: 'POST'
+            });
+          } else {
+            logError(`missing job url_path for job number ${dataItem.job_id}`);
+          }
+        });
+      }
+      return NextResponse.json({ status: 'ok' });
     }
-    return NextResponse.json({ status: 'ok' });
+  } catch (e) {
+    logError('an unknown error occured trying process failed jobs', e);
+    return NextResponse.error();
   }
 }
