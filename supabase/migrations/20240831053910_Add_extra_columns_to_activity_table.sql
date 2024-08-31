108f7cce433704095337ef5a6f596dac65a4b657
Stuart Clark <sjclark76@gmail.com>
2024-08-31 17:40:25 +1200

    feat:add extra activity columns

---
+++ supabase/migrations/20240831053910_Add_extra_columns_to_activity_table.sql
@@ -0,0 +1,35 @@
+alter table "public"."activities" add column "achievement_count" integer;
+
+alter table "public"."activities" add column "athlete_count" integer;
+
+alter table "public"."activities" add column "average_speed" real;
+
+alter table "public"."activities" add column "average_watts" real;
+
+alter table "public"."activities" add column "comment_count" integer;
+
+alter table "public"."activities" add column "device_watts" real;
+
+alter table "public"."activities" add column "distance" real;
+
+alter table "public"."activities" add column "elapsed_time" bigint;
+
+alter table "public"."activities" add column "elev_high" real;
+
+alter table "public"."activities" add column "elev_low" real;
+
+alter table "public"."activities" add column "kilojoules" real;
+
+alter table "public"."activities" add column "kudos_count" integer;
+
+alter table "public"."activities" add column "max_speed" real;
+
+alter table "public"."activities" add column "max_watts" real;
+
+alter table "public"."activities" add column "timezone" character varying;
+
+alter table "public"."activities" add column "total_elevation_gain" real;
+
+alter table "public"."activities" add column "weighted_average_watts" real;
+
+
